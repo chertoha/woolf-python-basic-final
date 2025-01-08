929cb2b19be23b6ca82c345ef2240748b47fa350
Oleksandr <alexandrenko3006@gmail.com>
2025-01-07 20:19:50 -0500

    Create methons in class Note_book and create handler-func for adding note

--- src/handlers/note_book/add_note.py
+++ src/handlers/note_book/add_note.py
@@ -1,7 +1,12 @@
 from typing import List
 from src.decorators.catch import catch
-
-
+from src.models.organizer import organizer_instance
 @catch
 def add_note(args: List[str]):
-    print("add_note")
+    if len(args) > 2:
+        raise ValueError("You must provide both a title and text")
+    title = args[0]
+    text = args[1]
+
+    organizer_instance.note_book.add_note(title, text)
+    return f"Note with title '{title}' successfully added."
--- src/models/note_book/note_book.py
+++ src/models/note_book/note_book.py
@@ -8,16 +8,52 @@ class NoteBook:
         self.__notes: List[Note] = []
 
     def add_note(self, title: str, text: str) -> None:
-        pass
+        if not title or not text:
+            raise ValueError("Title or text cannot be empty")
+        if any((note.title == title for note in self.__notes)):
+            raise KeyError(f"This title - {title} already exists")
+        
+        self.__notes.append(Note(title, text))
+       
 
     def find_note(self, searched_title: str):
-        pass
+        note = next((note for note in self.__notes if note.title == searched_title), None)
+
+        if note:
+            return note
+        else:
+            raise ValueError(f"Note with title - {searched_title} not exist")
 
     def remove_note(self, searched_title: str):
-        pass
+        for index, note in enumerate(self.__notes):
+            if note.title == searched_title:
+                del self.__notes[index]
+                return f"Note with title - {searched_title} removed successfully."
+            
+        raise ValueError(f"Note with title - {searched_title} does not exist")
 
     def update_note(self, searched_title: str, new_text: str):
-        pass
+        if not searched_title or not new_text:
+            raise ValueError("Title and new text cannot be empty")
+        
+        for  note in self.__notes:
+            if note.title == searched_title:
+                note.text = new_text
+                return f"Note with title - {searched_title} updated successfully."
+            
+        raise KeyError(f"Note with title - {searched_title} does not exist.")
+                    
+            
 
     def update_note_title(self, old_title: str, new_title: str):
-        pass
+        if not old_title or not new_title:
+            raise ValueError("Old title or new title cannott be empty")
+        
+        if any(note.title == new_title for note in self.__notes):
+            raise KeyError(f"New title - {new_title} already exists")
+        
+        for note in self.__notes:
+            if note.title == old_title:
+                note.title == new_title
+                return f"Title in note - updated on {new_title} successfully"
+        raise KeyError(f"Note with title - {old_title} does not exist.")
