d73f9390093281f12096ecf40b1c9e91ec10ab5d
Anton <a.chertok@akatech.com.ua>
2025-01-10 23:45:52 +0000

    Update show_help.py

--- src/handlers/show_help.py
+++ src/handlers/show_help.py
@@ -1,12 +1,16 @@
-from typing import List
 from src.decorators.catch import catch
 from src.cli.commands import commands
+from colorama import Fore, Style
 
 
 @catch
 def show_help():
 
-    print("Available commands:")
+    print("\n\033[1mAvailable commands:\033[0m\n")
     for command, details in commands.items():
         description = details.get("description", "No description available")
-        print(f"- {command.value}: {description}")
+        # print(f"- {command.value}: {description}")
+        # print("{:<30}{:<100}".format(command.value, description))
+        # print("{:<30}{:<100}".format(command.value, description))
+        print(f"{Fore.GREEN}\033[1m{command.value}\033[0m")
+        print(f"\033[3m{description}\033[0m\n")
