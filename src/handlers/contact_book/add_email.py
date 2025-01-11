d4e31b466f85effd8bd9129ca5e375f64e2ae93b
Andrii Salii <ansaliy200013@gmail.com>
2025-01-11 20:20:01 +0000

    feat email

--- src/handlers/contact_book/add_email.py
+++ src/handlers/contact_book/add_email.py
@@ -1,7 +1,24 @@
 from typing import List
+from src.helpers.logger import Logger
+from src.models.contact_book.email import Email
+from src.exceptions.non_existed_record_exception import NonExistedException
+from src.models.organizer import contact_book
+from src.exceptions.wrong_arguments_number_exception import WrongArgumentsNumberException
 from src.decorators.catch import catch
 
 
 @catch
 def add_email(args: List[str]):
-    print("add_email")
+    if (len(args) < 2):
+        raise WrongArgumentsNumberException(2)
+
+    name, email = args
+
+    record = contact_book.find_record(name)
+
+    if record == None:
+        raise NonExistedException(name)
+
+    record.email = Email(email)
+
+    Logger.success("Email addition was successful.")
--- src/handlers/contact_book/clear_email.py
+++ src/handlers/contact_book/clear_email.py
@@ -1,9 +1,25 @@
 from typing import List
 from src.decorators.catch import catch
 from src.decorators.confirm import confirm
+from src.exceptions.non_existed_record_exception import NonExistedException
+from src.exceptions.wrong_arguments_number_exception import WrongArgumentsNumberException
+from src.models.organizer import contact_book
+from src.helpers.logger import Logger
 
 
 @catch
 @confirm("Are you sure you want to clear email?")
 def clear_email(args: List[str]):
-    print("clear_email")
+    if (len(args) < 1):
+        raise WrongArgumentsNumberException(1)
+
+    name, = args
+
+    record = contact_book.find_record(name)
+
+    if record == None:
+        raise NonExistedException(name)
+
+    record.email = None
+
+    Logger.success("Email successfully cleared")
