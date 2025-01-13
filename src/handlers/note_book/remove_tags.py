9f16381e9e61bffcdedca50e0cff76d07c727c08
Anton <a.chertok@akatech.com.ua>
2025-01-13 12:44:14 +0000

    fix: remove-tags

--- src/handlers/note_book/remove_tags.py
+++ src/handlers/note_book/remove_tags.py
@@ -9,10 +9,13 @@ from src.decorators.confirm import confirm
 @catch
 @confirm("Are you sure you want to delete these tags?")
 def remove_tags(args: List[str]):
-    if len(args) != 2:
+    if len(args) < 2:
         raise WrongArgumentsNumberException(2)
+
     title, *tags = args
+
     note = note_book.find_note(title)
+
     if note:
         tags_set = set(tags)
         common_tags = tags_set.intersection(note.tags)
